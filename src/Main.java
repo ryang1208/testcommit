2182b22890b5a60865a131265643d16e2d749cea
yangrichard <3327959823@qq.com>
2019-03-28 16:28:02 +0800

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int a=1+2;
         int b=3;
-        int c = 3;
+        int c=3;
         System.out.println("Hello World!");
     }
 }
