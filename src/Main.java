60dfdedb7bb6272c35232151e26efedb1308c82a
yangrichard <3327959823@qq.com>
2019-03-28 16:31:39 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
 
-    public static void main(String[] args) {
-        int a=1+2;
-        int b=3;
-        int c=3;
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    int a = 1 + 2;
+    int b = 3;
+    int c = 3;
+    int d = 4;
+    System.out.println("Hello World!");
+  }
 }
